5f7c795c9c62dc0ec6bbb217ede68773b12a06e8
fatih <fatih@gmail.com>
2022-10-04 16:44:44 +0300

    Aciklama kısmı gerceklestirilecek

--- lib/sayfalar/Anasayfa.dart
+++ lib/sayfalar/Anasayfa.dart
@@ -19,21 +19,87 @@ class _AnasayfaState extends State<Anasayfa> {
             width: MediaQuery.of(context).size.width,
             child: Column(
               children: [
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(
-                    color: HexColor("FBA346"),
+                buildSeritBanner(context),
+                /** ANASAYFA ARAMA KISMI **/
+                buildUstAramaForm(),
+                /** ANASYAFA SLIDER KISMI **/
+                buildSlider(),
+                /** CATEGORIES KISMI **/
+                Padding(
+                  padding: EdgeInsets.only(top: 20,left: 20,right: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Categories",style: TextStyle(fontSize: 18,fontWeight: FontWeight.w500)),
+                      Text("View All",style: TextStyle(color: HexColor("FBA346"),fontSize: 18,fontWeight: FontWeight.w500))
+                    ],
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 5),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 20),
+                  child: Container(
+                    height: 102,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
                       children: [
-                        Icon(Icons.verified_user_rounded,color: Colors.white),
-                        SizedBox(
-                          width: 10,
+                        Padding(
+                            padding: const EdgeInsets.only(right: 22),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(10),
+                                      boxShadow: [
+                                        BoxShadow(
+                                            color: HexColor("bdbdbd"),
+                                            blurRadius:10,
+                                            offset: Offset(0,4)
+                                        )
+                                      ]
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(15),
+                                    child: Text("asds"),
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text("All")
+                              ],
+                            )
                         ),
-                        Text("Best Safety Standards amid Covid -19",style: TextStyle(color: Colors.white),textAlign: TextAlign.center),
+                        Padding(
+                            padding: const EdgeInsets.only(right: 22),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(10),
+                                      boxShadow: [
+                                        BoxShadow(
+                                            color: HexColor("bdbdbd"),
+                                            blurRadius:10,
+                                            offset: Offset(0,4)
+                                        )
+                                      ]
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(15),
+                                    child: Text("asds"),
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text("All")
+                              ],
+                            )
+                        ),
                       ],
                     ),
                   ),
@@ -66,6 +132,152 @@ class _AnasayfaState extends State<Anasayfa> {
           ),
         )
       ],
+    );
+  }
+
+  /** SERIT BANNER KISMI **/
+  Container buildSeritBanner(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        color: HexColor("FBA346"),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 5),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.verified_user_rounded,color: Colors.white),
+            SizedBox(
+              width: 10,
+            ),
+            Text("Best Safety Standards amid Covid -19",style: TextStyle(color: Colors.white),textAlign: TextAlign.center),
+          ],
+        ),
+      ),
+    );
+  }
+
+  /** UST ARAMA FORM **/
+  Padding buildUstAramaForm() {
+    return Padding(
+      padding: EdgeInsets.all(20),
+      child: Container(
+        child: Row(
+          children: [
+            Expanded(
+              flex :3,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                          color: HexColor("bdbdbd"),
+                          blurRadius:10,
+                          offset: Offset(0,4)
+                      )
+                    ]
+                ),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.search),
+                      hintText: "Search",
+                      border: InputBorder.none
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                          color: HexColor("bdbdbd"),
+                          blurRadius:10,
+                          offset: Offset(0,4)
+                      )
+                    ]
+                ),
+                child:Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 12,horizontal: 17),
+                  child: Icon(Icons.tune),
+                )
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  /** ANASAYFA SLIDER KISMI **/
+  Padding buildSlider() {
+    return Padding(
+      padding: EdgeInsets.only(top: 20,bottom: 20,left: 20),
+      child: Container(
+        height: 152,
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(right: 15),
+              child: AspectRatio(
+                aspectRatio: 4/2.25,
+                child: Container(
+                  height: 152,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(top: 21,left: 16),
+                        child: Text("January Offer",style: TextStyle(fontSize: 28,fontWeight: FontWeight.w500)),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 17,left: 16),
+                        child: Text("January Offer",style: TextStyle(fontSize: 18,fontWeight: FontWeight.w500)),
+                      ),
+                    ],
+                  ),
+                  decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(10)
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 15),
+              child: AspectRatio(
+                aspectRatio: 4/2.25,
+                child: Container(
+                  height: 152,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(top: 21,left: 16),
+                        child: Text("January Offer",style: TextStyle(fontSize: 28,fontWeight: FontWeight.w500)),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 17,left: 16),
+                        child: Text("January Offer",style: TextStyle(fontSize: 18,fontWeight: FontWeight.w500)),
+                      ),
+                    ],
+                  ),
+                  decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(10)
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
